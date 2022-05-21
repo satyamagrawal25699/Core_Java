03914291ca627e2fc215623bb7dc2ffedb2b5cf5
satyamagrawal25699 <86581208+satyamagrawal25699@users.noreply.github.com>
2022-05-21 21:50:33 +0530

    Collectiondemo

---
+++ Core_Java/src/Util_Package_Exaamples/ColDemo2.java
@@ -0,0 +1,34 @@
+package Util_Package_Exaamples;
+import java.util.ArrayList;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class ColDemo2 {
+	public static void main(String[] args) {
+		List<Integer> nlist=new ArrayList<>();
+		nlist.add(10);
+		nlist.add(50);
+		nlist.add(5);
+		nlist.add(400);
+		nlist.add(20);
+		nlist.add(1000);
+		System.out.println(nlist);
+		
+		Collections.sort(nlist);
+		System.out.println(nlist);
+		
+		Collections.sort(nlist,new Mycomparator());
+		System.out.println(nlist);
+		
+	}
+}
+
+class Mycomparator implements Comparator<Integer>{
+	@Override
+	public int compare(Integer o1,Integer o2) {
+		return o2.compareTo(o1);
+		
+	}
+}
